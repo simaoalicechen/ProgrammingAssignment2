97ad783f6d7379f4273c8e6522ffa61f5b5e315c
simaoalicechen <67714084+simaoalicechen@users.noreply.github.com>
2020-07-29 17:45:09 -0400

    Update cachematrix.R
    I did this project for the Peer-graded Assignment: Programming Assignment 2: Lexical Scoping on R-programming course on Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrx <- function(x = matrix()) {
+        
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) iniv <<- inverse
+  getInverse <- function() iniv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  iniv <- x$getInverse()
+  if (!is.null(iniv)) {
+    message("getting cached data")
+    return(iniv)
+  }
+  mat <- x$get()
+  iniv <- solve(mat, ...)
+  x$setInverse(iniv)
+  iniv
 }
